5a96ac70669c022b0d28a040fdebc7f6897589d7
saifkhan26 <saifuddinkhan1226@gmail.com>
2023-07-09 10:26:15 +0500

    Project Setup

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-rich-black">
+      <div className="h-96 flex items-center justify-center">
+        <h1 className="text-6xl font-black">Ecommerce Web App</h1>
+      </div>
+    </main>
+  );
+}
