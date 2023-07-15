38620e86778e7bcd29a4615fc2c4d34a9bcd9c3c
saifkhan26 <saifuddinkhan1226@gmail.com>
2023-07-15 12:02:03 +0500

    Home =^> Product Card

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 
 export default function ProductCard({
   brand,
@@ -8,5 +9,20 @@ export default function ProductCard({
   name,
   price,
 }) {
-  return <div className="bg-white">{name}</div>;
+  return (
+    <div className="bg-white rounded-3xl p-7">
+      <div className="h-60 relative bg-bright-rose rounded-3xl">
+        <Image src={''} layout="fill"/>
+      </div>
+      <div className="grid grid-cols-3 mt-6">
+        <div className="col-span-2">
+          <p className="text-2xl mb-4">{name}</p>
+          <p className="text-sm">{description}</p>
+        </div>
+        <div className="self-end justify-self-end">
+          <button className="btn-primary">Add To Cart</button>
+        </div>
+      </div>
+    </div> 
+  );
 }
