4f9443cf983e413c500a38e5c0b4849f7db4d59c
saifkhan26 <saifuddinkhan1226@gmail.com>
2023-07-15 12:55:09 +0500

    Product Post Handler

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,5 +1,15 @@
 import { connectToDB } from "@utils/database";
+import { NextResponse } from "next/server";
 import Product from "@models/product";
+import multer from "multer";
+
+const upload = multer({dest: "uploads/"})
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
 
 export async function GET(req, res) {
   try {
@@ -9,6 +19,31 @@ export async function GET(req, res) {
   } catch (error) {
     return new Response("Failed to Fetch all Products", { status: 500 });
   }
+}
+export async function POST(req, res) {
+  try {
+    await connectToDB()
+    const uploadImage = upload.single('image')
 
-  return new Response(JSON.stringify(products));
+    await new Promise((resolve, reject) => {
+      uploadImage(req, res, (err) => {
+        if(err) {
+          return reject(err);
+        }
+        resolve();
+      })
+    })
+
+    const newProduct = new Product({
+      ...req.body,
+      image: req.file.path
+    })
+
+    const data = await newProduct.save();
+
+    return res.status(201).json({success: true, message: 'Product Created Successfully', data: data})
+    
+  } catch (error) {
+     return res.status(500).json({ success: false, error: 'Failed to create product' });
+  }
 }
