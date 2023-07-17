7ffcf6d2757bca03a93620d9527098dba341452a
saifkhan26 <saifuddinkhan1226@gmail.com>
2023-07-17 05:17:29 +0500

    Name changed

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -9,6 +9,32 @@ export async function GET(req, res) {
   } catch (error) {
     return new Response("Failed to Fetch all Products", { status: 500 });
   }
+}
 
-  return new Response(JSON.stringify(products));
+export async function POST(req, res) {
+  try {
+    await connectToDB()
+    const uploadImage = upload.single('image')
+
+    await new Promise((resolve, reject) => {
+      uploadImage(req, res, (err) => {
+        if(err) {
+          return reject(err);
+        }
+        resolve();
+      })
+    })
+
+    const newProduct = new Product({
+      ...req.body,
+      image: req.file.path
+    })
+
+    const data = await newProduct.save();
+
+    return res.status(201).json({success: true, message: 'Product Created Successfully', data: data})
+    
+  } catch (error) {
+     return res.status(500).json({ success: false, error: 'Failed to create product' });
+  }
 }
